614c70f8fc2f72fa56437abb4a9b01b91c13764c
demo-23home <zeyadslama23@gmail.com>
2023-11-26 17:53:47 +0200

    passing(data,functions) via props

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -2,8 +2,18 @@ import ListGroup from "./components/ListGroup";
 import { MouseEvent, useState } from "react";
 function App() {
   let items = ["New York", "San Francissco", "Tokyo", "London", "Paris"];
-
-  return <div><ListGroup items={items} heading="Cities"/></div>
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+  return (
+    <div>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+    </div>
+  );
 }
 
 export default App;
