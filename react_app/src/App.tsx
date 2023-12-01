36e9355de8c2ee1e0bd9cc67fabfc27eb0c54d70
demo-23home <zeyadslama23@gmail.com>
2023-12-01 13:43:33 +0200

    Building forms, handling Submittion

--- react_app/src/App.tsx
+++ react_app/src/App.tsx
@@ -1,22 +1,10 @@
 import { useState } from "react";
-import { ExpandbleText } from "./components/ExpandbleText";
+import { Form } from "./components/Form";
 
 function App() {
   return (
     <div>
-      <ExpandbleText maxChars={10}>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti, aut
-        quisquam doloribus aliquid accusamus excepturi harum soluta, debitis
-        minima itaque quis sint expedita magnam sequi est iure omnis voluptas!
-        Minima, quam ut deserunt ad dolor cumque dicta inventore maxime at
-        laboriosam quia, nam earum necessitatibus nemo saepe adipisci iste
-        explicabo hic error libero! Odio vel beatae reiciendis? Tenetur minus
-        consectetur consequatur impedit laboriosam doloremque, doloribus et
-        sequi, perspiciatis eos voluptatibus, dolor ex officia! Porro eaque quos
-        ipsum deserunt expedita totam, suscipit enim. Eveniet officia corporis
-        exercitationem magnam sit molestiae, fugiat, porro omnis in similique
-        placeat eaque commodi quaerat at qui.
-      </ExpandbleText>
+      <Form></Form>
     </div>
   );
 }
---
+++ react_app/src/components/Form.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { FormEvent } from "react";
+
+export const Form = () => {
+  const handleSubmit = (event:FormEvent) => {
+    event.preventDefault();
+    console.log("Submitted");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" type="text" className="form-control" />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
